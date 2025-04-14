3459b32df58b08c33cd4a8e31d472e80646e8b83
catalogo12 <96148687+catalogo12@users.noreply.github.com>
2025-04-14 17:26:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! hoy 5 octubre 24')
+  res.send('Hello World! hoy 14 abri 2025')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} hoy 14 abril 2025`)
+  console.log(`Example app listening on port ${port} `)
 })
 
